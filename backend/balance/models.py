139f7161459e5fe38c33b9b74b0499dc6147c5cd
Piotr Sierota <sierota.piotr@gmail.com>
2020-04-25 19:25:50 +0200

    All declared columns exist in DB now.

--- backend/balance/models.py
+++ backend/balance/models.py
@@ -4,33 +4,33 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=20)
     surname = models.CharField(max_length=20)
-    enabled = models.BooleanField
-    balance = models.FloatField
+    enabled = models.BooleanField(default=True)
+    balance = models.FloatField(default=0)
 
 
 class Car(models.Model):
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
-    enabled = models.BooleanField
-    average_cost = models.FloatField
+    enabled = models.BooleanField(default=True)
+    average_cost = models.FloatField(default=0)
 
 
 class Ride(models.Model):
-    entry_date = models.DateField
-    date = models.DateField
-    distance = models.IntegerField
+    entry_date = models.DateField(null=True)
+    date = models.DateField(null=True)
+    distance = models.IntegerField(default=0)
     car = models.ForeignKey(Car, related_name='rides', on_delete=models.CASCADE)
     users = models.ManyToManyField(User)
 
 
 class Refuel(models.Model):
-    date = models.DateField
-    price = models.IntegerField
+    date = models.DateField(null=True)
+    price = models.IntegerField(default=0)
     car = models.ForeignKey(Car, related_name='refuels', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='refuels', on_delete=models.CASCADE)
 
 
 class Transaction(models.Model):
-    amount = models.IntegerField
+    amount = models.IntegerField(default=0)
     payer = models.ForeignKey(User, related_name='outgoing_transactions', on_delete=models.CASCADE)
     payee = models.ForeignKey(User, related_name='incoming_transactions', on_delete=models.CASCADE)
