cc521df67491ab701440643fc8cb20e8aed8a237
Piotr Sierota <sierota.piotr@gmail.com>
2020-04-27 10:02:16 +0200

    Models string representation. Models managable via admin site.

--- backend/balance/models.py
+++ backend/balance/models.py
@@ -7,12 +7,18 @@ class User(models.Model):
     enabled = models.BooleanField(default=True)
     balance = models.FloatField(default=0)
 
+    def __str__(self):
+        return "{name} {surname}".format(name=self.name, surname=self.surname)
+
 
 class Car(models.Model):
     make = models.CharField(max_length=20)
     model = models.CharField(max_length=20)
     enabled = models.BooleanField(default=True)
     average_cost = models.FloatField(default=0)
+
+    def __str__(self):
+        return "{make} {model}".format(make=self.make, model=self.model)
 
 
 class Ride(models.Model):
@@ -22,6 +28,9 @@ class Ride(models.Model):
     car = models.ForeignKey(Car, related_name='rides', on_delete=models.CASCADE)
     users = models.ManyToManyField(User)
 
+    def __str__(self):
+        return "{distance} km at {entry_date}".format(distance=self.distance, entry_date=self.entry_date)
+
 
 class Refuel(models.Model):
     date = models.DateField(null=True)
@@ -29,8 +38,14 @@ class Refuel(models.Model):
     car = models.ForeignKey(Car, related_name='refuels', on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='refuels', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{car} at {date} for {price} PLN".format(car=str(self.car), date=self.date, price=self.price)
+
 
 class Transaction(models.Model):
     amount = models.IntegerField(default=0)
     payer = models.ForeignKey(User, related_name='outgoing_transactions', on_delete=models.CASCADE)
     payee = models.ForeignKey(User, related_name='incoming_transactions', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{amount} PLN from {payer} to {payee}".format(amount=self.amount, payer=self.payer, payee=self.payee)
