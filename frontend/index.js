9e624fb0e2ea782706a0b2c02edace10d2b56373
Paweł Grabowski <pawel.grabowski5@gmail.com>
2020-04-30 23:44:39 +0200

    basics on home view
     - change dev stack to: Vue.js and materialize.css
     - create basic components on home view

---
+++ frontend/index.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        appName: 'Fuel Up',
+        appVersion: '0.0.3',
+        message: 'Hello Vue!'
+    }
+});
---
+++ frontend/js/app/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+   let fab = document.querySelector('#_fab')
+   let Fab = M.FloatingActionButton.init(fab, {
+       direction: 'top',
+       hoverEnabled: false
+   })
+
+   // Fab.open()
+})
