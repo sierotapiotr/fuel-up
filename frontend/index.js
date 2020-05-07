8caebbdb7f76b566d7e24bc18782d893a344baec
Paweł Grabowski <pawel.grabowski5@gmail.com>
2020-05-07 08:38:59 +0200

    Cleanup some mess...

--- frontend/index.js
+++
@@ -1,8 +0,0 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        appName: 'Fuel Up',
-        appVersion: '0.0.3',
-        message: 'Hello Vue!'
-    }
-});
--- frontend/js/app/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-	let balanceNode = document.querySelector('#balance')
-	let balanceValue = Number(balanceNode.getAttribute('value'))
-
-	if (balanceValue < 0) balanceNode.classList.add('text-danger')
-	if (balanceValue == 0) balanceNode.classList.add('text-warning')
-	if (balanceValue > 0) balanceNode.classList.add('text-success')
-	balanceNode.innerText = balanceValue + ' zł'
-})
