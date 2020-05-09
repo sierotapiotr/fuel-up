e29735cf7c8370130fdb2ea1a10f5e24bc759130
Paweł Grabowski <pawel.grabowski5@gmail.com>
2020-05-09 17:33:15 +0200

    build 20.0509.1732
     - dodany formularz tankowania

--- frontend/src/views/Refuel.vue
+++ frontend/src/views/Refuel.vue
@@ -1,7 +1,47 @@
 <template>
 	<div>
 		<Navbar />
-		<h1>Refuel</h1>
+		<form method="POST" action="" novalidate>
+			<div class="form-group">
+				<label for="refuelUser">Osoba tankująca</label>
+				<select name="refuelUser" class="custom-select" id="refuelUser" required>
+					<option selected disabled value="">Wybierz...</option>
+					<option value="1">P</option>
+					<option value="2">D</option>
+					<option value="3">T</option>
+				</select>
+			</div>
+			<div class="form-group">
+				<label for="vehicle">Pojazd</label>
+				<select name="vehicle" class="custom-select" id="vehicle" required>
+					<option selected disabled value="">Wybierz...</option>
+					<option value="1">P</option>
+					<option value="2">D</option>
+					<option value="3">T</option>
+				</select>
+			</div>
+			<p class="text-muted">Typ paliwa:</p>
+			<div class="btn-group btn-group-toggle w-100 mb-3" role="group" data-toggle="buttons" disabled>
+				<label class="btn btn-outline-secondary w-50">
+					<input type="radio" name="fuelType" id="1" disabled>Benzyna/Olej
+				</label>
+				<label class="btn btn-outline-secondary w-50">
+					<input type="radio" name="fuelType" id="2" disabled>Gas
+				</label>
+			</div>
+			<div class="form-group">
+				<label for="amount">Ilość (l)</label>
+				<input name="amount" type="number" id="amount" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="price">Kwota (zł)</label>
+				<input name="price" type="number" id="price" class="form-control" required>
+				<div class="invalid-feedback">
+					Please choose a username.
+				</div>
+			</div>
+			<button type="submit" class="btn btn-secondary w-100 fixed-bottom rounded-0">Zapisz</button>
+		</form>
 	</div>
 </template>
 
