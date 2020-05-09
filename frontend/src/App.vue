2ccceed591cb34aa0ba6564213cc455f90fec8f5
Paweł Grabowski <pawel.grabowski5@gmail.com>
2020-05-09 17:35:29 +0200

    build 20.0509.1734
     - reorganizacja komponentów i widoków

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,24 @@
 <template>
-	<router-view></router-view>
+	<div id="main">
+		<Navbar/>
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
+	import Navbar from "./components/Navbar";
 
 	export default {
 		name: 'Fuel-up',
-		components: {}
+		components: { Navbar }
 	}
 </script>
+
+<style>
+	#main {
+		width: 100%;
+		min-width: 100vw;
+		height: 100%;
+		min-height: 100vh;
+	}
+</style>
