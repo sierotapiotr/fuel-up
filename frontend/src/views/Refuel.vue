d7472a7800e964b29e1c17f8365fbfacf242ab6f
Paweł Grabowski <pawel.grabowski5@gmail.com>
2020-05-09 17:00:46 +0200

    build 20.0905.1700
     - dodany widok *refuel*

---
+++ frontend/src/views/Refuel.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<Navbar />
+		<h1>Refuel</h1>
+	</div>
+</template>
+
+<script>
+	import Navbar from "../components/Navbar";
+	export default {
+		name: "Refuel",
+		components: { Navbar }
+	}
+</script>
+
+<style scoped>
+
+</style>
