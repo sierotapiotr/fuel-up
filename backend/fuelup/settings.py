7f8185cd708989854d511d45a8f5ee9a56b2a869
Piotr Sierota <sierota.piotr@gmail.com>
2020-06-06 19:06:29 +0200

    Added django rest framework

--- backend/fuelup/settings.py
+++ backend/fuelup/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'balance.apps.BalanceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
